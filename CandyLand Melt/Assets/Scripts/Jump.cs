d3cca3e635594f4782ddeeedcf8da352674de3a8
benjallauro <benjallauro@gmail.com>
2023-06-16 16:10:17 -0300

    The player can now deliver pickables to citizens

--- CandyLand Melt/Assets/Scripts/Jump.cs
+++ CandyLand Melt/Assets/Scripts/Jump.cs
@@ -6,17 +6,24 @@ namespace Player
     {
         [Range(1, 10)] public float jumpVelocity = 5;
         private Rigidbody2D rb;
+        private bool _jumpAvailable = true;
 
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
         }
 
+        public void SetJumpAvailable(bool jumpAvailable)
+        {
+            _jumpAvailable = jumpAvailable;
+        }
+
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && _jumpAvailable)
             {
                 rb.velocity = Vector2.up * jumpVelocity;
+                _jumpAvailable = false;
             }
         }
     }
