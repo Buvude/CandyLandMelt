69a570d81b78d9946c6d7464bf363197074ddd91
Hollowriter <rodrigodiazklipphan@gmail.com>
2023-06-19 16:16:43 -0300

    Starting the moving platform

---
+++ CandyLand Melt/Assets/Scripts/StageElements/MovingPlatform.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float PlatformSpeed;
+    [SerializeField] private float PlatformTimer;
+    [SerializeField] private float directionHorizontal;
+    [SerializeField] private float directionVertical;
+    [SerializeField] private float angle;
+}
