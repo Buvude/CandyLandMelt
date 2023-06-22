4e55501509057dcb2e8f3b6da1504fcd04d66f27
benjaminllauro <benjallauro@gmail.com>
2023-06-22 01:50:05 -0300

    All the game scenes now have music

--- CandyLand Melt/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ CandyLand Melt/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,12 +13,16 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID STARTMUSIC = 3827058668U;
+        static const AkUniqueID STARTGAMEOVERMUSIC = 3903099812U;
+        static const AkUniqueID STARTGAMEPLAYMUSIC = 1047651740U;
+        static const AkUniqueID STARTMENUMUSIC = 2440264179U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GAME_OVER_MUSIC_SOUNDBANK = 532728782U;
+        static const AkUniqueID MENU_MUSIC_SOUNDBANK = 1311041530U;
         static const AkUniqueID MUSICSOUNDBANK = 3427827313U;
     } // namespace BANKS
 
